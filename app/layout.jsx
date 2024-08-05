9482621b3fc50f0bdb7c5b891f937ceb67947cb8
MalithaJeewaka <144432834+MalithaJeewaka@users.noreply.github.com>
2024-08-05 21:29:22 +0530

    created layout and homepage

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "@/assets/styles/global.css";
+
+export const metadata = {
+  title: "PropertyPulse | Find The Perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
+}; // meta data
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
